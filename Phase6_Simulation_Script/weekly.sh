0bab43f0dedf4ee10cd51818cf1178578917f480
Chris Chen <chris.chen1@ontariotechu.net>
2025-04-16 20:16:59 -0400

    Fixed weekly script file handling

--- Phase6_Simulation_Script/weekly.sh
+++ Phase6_Simulation_Script/weekly.sh
@@ -1,12 +1,11 @@
+cp ../OldMasterBankAccounts.txt ../OldMasterBankAccounts_p6.txt
+cp ../CurrentBankAccounts.txt ../CurrentBankAccounts_p6.txt
+
 for DAY in 1 2 3 4 5 6 7
 do
-    #read based off how daily.sh is handling directories
-    #export
-
-    echo "Day: $DAY"
-    ./daily.sh
-
-    cp ../OldMasterBankAccounts.txt "../OldMasterBankAccounts${DAY}.txt"
-    cp ../CurrentBankAccounts.txt "../CurrentBankAccounts${DAY}.txt"
+    echo "Start of day: $DAY"
+    ./daily.sh $DAY
+    echo "End of day: $DAY"
 done
+
 echo "Finished weekly simulation."
